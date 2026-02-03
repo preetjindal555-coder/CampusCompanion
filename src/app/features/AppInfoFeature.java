6fbeded25c43051495ba19ff40bf044d51f4fa51
preet jindal <preetjindal555@gmail.com>
2026-02-03 16:14:19 -0500

    Test change final

--- src/app/features/AppInfoFeature.java
+++ src/app/features/AppInfoFeature.java
@@ -4,18 +4,18 @@ import java.time.LocalDateTime;
 
 public class AppInfoFeature {
     public static String run() {
-       return "Campus Companion\n"
-             + "---------------------------------\n"
-             + "Campus Companion is a simple desktop\n"
-             + "application designed to display\n"
-             + "useful and interesting information\n"
-             + "for students in one place.\n\n"
-             + "The app provides study tips,\n"
-             + "motivational messages, and fun facts\n"
-             + "through a clean and easy-to-use\n"
-             + "graphical interface.\n\n"
-             + "Version: 1.0\n"
-             + "Platform: Java Desktop Application\n"
-             + "---------------------------------\n";
+        return "Campus Companion\n"
+                + "---------------------------------\n"
+                + "Campus Companion is a simple desktop\n"
+                + "application designed to display\n"
+                + "useful and interesting information\n"
+                + "for students in one place.\n\n"
+                + "The app provides study tips,\n"
+                + "motivational messages, and fun facts\n"
+                + "through a clean and easy-to-use\n"
+                + "graphical interface.\n\n"
+                + "Version: 1.0\n"
+                + "Platform: Java Desktop Application\n"
+                + "---------------------------------\n";
     }
 }
