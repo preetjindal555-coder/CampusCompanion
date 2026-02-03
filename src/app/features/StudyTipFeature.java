8a6fcd8f446b35f98b043f8533890c60d3aaa6c3
preet jindal <preetjindal555@gmail.com>
2026-02-03 16:22:19 -0500

    change in study tip

--- src/app/features/StudyTipFeature.java
+++ src/app/features/StudyTipFeature.java
@@ -2,12 +2,12 @@ package app.features;
 
 public class StudyTipFeature {
     public static String run() {
-        return "Study Tip\n"
-             + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Study Style: YOUR STUDY STYLE HERE\n"
-             + "Tip:\n"
-             + "Replace this text with your own study tip.\n"
-             + "------------------------------\n";
+        return "Study Tip or any fun facts \n"
+                + "------------------------------\n"
+                + "Student Name: YOUR NAME HERE\n"
+                + "Study Style: YOUR STUDY STYLE HERE\n"
+                + "Tip:\n"
+                + "Replace this text with your own study tip or any fun facts .\n"
+                + "------------------------------\n";
     }
 }
