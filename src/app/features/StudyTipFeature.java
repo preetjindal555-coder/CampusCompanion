7aba7e360638d9c1a100bbb25774233ad3129d58
janvipatel2101 <92202976+janvipatel2101@users.noreply.github.com>
2026-02-03 23:30:50 -0500

    final change

--- src/app/features/StudyTipFeature.java
+++ src/app/features/StudyTipFeature.java
@@ -4,8 +4,8 @@ public class StudyTipFeature {
     public static String run() {
         return "Study Tip\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Study Style: YOUR STUDY STYLE HERE\n"
+             + "Student Name: janvi patel\n"
+             + "Study Style: alone \n"
              + "Tip:\n"
              + "Replace this text with your own study tip.\n"
              + "------------------------------\n";
