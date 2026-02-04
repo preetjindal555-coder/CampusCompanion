2fe3adbea98a59dd4e4904d9cf06167cf0ea5141
Janvi patel <Janvipatel2119@gmail.com>
2026-02-03 23:02:05 -0500

    motivation

--- src/app/features/MotivationFeature.java
+++ src/app/features/MotivationFeature.java
@@ -4,9 +4,9 @@ public class MotivationFeature {
     public static String run() {
         return "Motivation\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
+             + "Student Name: Janvi Patel\n"
              + "What motivates you:\n"
-             + "Replace this text with your motivation.\n"
+             + "Ongoing Time\n"
              + "------------------------------\n";
     }
 }
